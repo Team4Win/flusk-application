712f246593f9f556171308f9d04297a941fdabd2
Cristian Budeanu <budeanu.cristian28@gmail.com>
2022-06-11 13:36:26 +0300

    code!

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+# importing Flask and other modules
+from flask import Flask, request, render_template
 
+# Flask constructor
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+# @app.route('/', methods=["GET"])
+# def hello_world():
+#     return "Hello, world!"
+
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/', methods=["GET", "POST"])
+def get_form():
+    if request.method == "POST":
+        # getting input with link = linkname in HTML form
+        link = request.form.get("link_name")
+        return "Your link is : " + link
+
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
