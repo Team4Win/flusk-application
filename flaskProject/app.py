c4f45e95585b779f5ff4cce2dd05f34cc19c8c48
Cristian Budeanu <budeanu.cristian28@gmail.com>
2022-06-11 16:56:42 +0300

    code! Updated

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,5 +1,9 @@
 # importing Flask and other modules
 from flask import Flask, request, render_template
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+
 
 # Flask constructor
 app = Flask(__name__)
@@ -17,8 +21,22 @@ def get_form():
     if request.method == "POST":
         # getting input with link = linkname in HTML form
         link = request.form.get("link_name")
-        return "Your link is : " + link
+        # making requests instance
+        reqs = requests.get(link)
 
+        # using the BeautifulSoup module
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+
+        # displaying the title
+        print("Title of the website is : ")
+        for title in soup.find_all('h1'):
+          print(title.get_text())
+        lista = []
+        for data in soup.find_all('p'):
+            lista.append(data.get_text())
+        response = title.get_text() + '\n'
+        response += "\n".join(lista)
+        return response
     return render_template("index.html")
 
 
---
+++ flaskProject/services/topic_classification_service.py
@@ -0,0 +1,8 @@
+def get_article_topic(headline, body):
+
+    # call_clasiffication_method
+
+
+
+
+    return "Topic"
